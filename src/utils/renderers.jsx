c278dcd428dba2dae0245a1bf3a8b9517831be63
Felix Allistar <147667330+FelixAllistar@users.noreply.github.com>
2025-10-24 18:16:20 -0400

    latex but its ugly. need to find styles.

--- src/utils/renderers.jsx
+++ src/utils/renderers.jsx
@@ -1,21 +1,7 @@
 import { Streamdown } from 'streamdown';
 
 export const renderMessage = (text) => (
-  <Streamdown
-    components={{
-      code({ node, inline, className, children, ...props }) {
-        return inline ? (
-          <code className={className} {...props}>
-            {children}
-          </code>
-        ) : (
-          <pre className={className} {...props}>
-            <code>{children}</code>
-          </pre>
-        );
-      },
-    }}
-  >
+  <Streamdown>
     {text}
   </Streamdown>
 );
